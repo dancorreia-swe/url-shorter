d57b5685d299eba20989d54ededa2572ca9d1a08
Daniel Correia <danielgalego.dev@gmail.com>
2024-04-04 11:29:35 -0300

    feat: add auth

--- lib/url_shorter_web/router.ex
+++ lib/url_shorter_web/router.ex
@@ -1,6 +1,8 @@
 defmodule UrlShorterWeb.Router do
   use UrlShorterWeb, :router
 
+  import UrlShorterWeb.UserAuth
+
   pipeline :browser do
     plug :accepts, ["html"]
     plug :fetch_session
@@ -8,6 +10,7 @@ defmodule UrlShorterWeb.Router do
     plug :put_root_layout, html: {UrlShorterWeb.Layouts, :root}
     plug :protect_from_forgery
     plug :put_secure_browser_headers
+    plug :fetch_current_user
   end
 
   pipeline :api do
@@ -18,15 +21,6 @@ defmodule UrlShorterWeb.Router do
     pipe_through :browser
 
     get "/", PageController, :home
-
-    live "/short_links", ShortLinkLive.Index, :index
-    live "/short_links/new", ShortLinkLive.Index, :new
-    live "/short_links/:id/edit", ShortLinkLive.Index, :edit
-
-    live "/short_links/:id", ShortLinkLive.Show, :show
-    live "/short_links/:id/show/edit", ShortLinkLive.Show, :edit
-
-    get "/:key", ShortLinkRedirectController, :index
   end
 
   # Other scopes may use custom stacks.
@@ -49,5 +43,51 @@ defmodule UrlShorterWeb.Router do
       live_dashboard "/dashboard", metrics: UrlShorterWeb.Telemetry
       forward "/mailbox", Plug.Swoosh.MailboxPreview
     end
+  end
+
+  ## Authentication routes
+
+  scope "/", UrlShorterWeb do
+    pipe_through [:browser, :redirect_if_user_is_authenticated]
+
+    live_session :redirect_if_user_is_authenticated,
+      on_mount: [{UrlShorterWeb.UserAuth, :redirect_if_user_is_authenticated}] do
+      live "/users/register", UserRegistrationLive, :new
+      live "/users/log_in", UserLoginLive, :new
+      live "/users/reset_password", UserForgotPasswordLive, :new
+      live "/users/reset_password/:token", UserResetPasswordLive, :edit
+    end
+
+    post "/users/log_in", UserSessionController, :create
+  end
+
+  scope "/", UrlShorterWeb do
+    pipe_through [:browser, :require_authenticated_user]
+
+    live_session :require_authenticated_user,
+      on_mount: [{UrlShorterWeb.UserAuth, :ensure_authenticated}] do
+      live "/users/settings", UserSettingsLive, :edit
+      live "/users/settings/confirm_email/:token", UserSettingsLive, :confirm_email
+
+      live "/short_links", ShortLinkLive.Index, :index
+      live "/short_links/new", ShortLinkLive.Index, :new
+      live "/short_links/:id/edit", ShortLinkLive.Index, :edit
+      live "/short_links/:id", ShortLinkLive.Show, :show
+      live "/short_links/:id/show/edit", ShortLinkLive.Show, :edit
+    end
+  end
+
+  scope "/", UrlShorterWeb do
+    pipe_through [:browser]
+
+    delete "/users/log_out", UserSessionController, :delete
+
+    live_session :current_user,
+      on_mount: [{UrlShorterWeb.UserAuth, :mount_current_user}] do
+      live "/users/confirm/:token", UserConfirmationLive, :edit
+      live "/users/confirm", UserConfirmationInstructionsLive, :new
+    end
+
+    get "/:key", ShortLinkRedirectController, :index
   end
 end
